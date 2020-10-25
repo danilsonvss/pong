fcd35c098103f8f1b11007282a841d065d351cb6
Danilson Veloso de Sousa Santos <danilsonvs04@gmail.com>
2020-10-24 22:21:40 -0300

    Refatoração
    Refatorando o projeto para inglês.

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody2D mRb;
+    public Vector2 mVelocidade;
+    public float velocidade = 6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Passando a velocidade
+        mVelocidade.x = velocidade;
+        mVelocidade.y = velocidade;
+
+        // Gerando um valor aleatorio
+        int random = Random.Range(0, 4);
+
+        // Gerando a direção aleatória da bola
+        switch(random)
+        {
+            // Superior direita
+            case 0:
+                mVelocidade.x = velocidade;
+                mVelocidade.y = velocidade;
+                break;
+
+            // Superior esquerda
+            case 1:
+                mVelocidade.x = -velocidade;
+                mVelocidade.y = -velocidade;
+                break;
+
+            // Inferior direita
+            case 2:
+                mVelocidade.y = -velocidade;
+                mVelocidade.x = velocidade;
+                break;
+
+            // Inferior esquerda
+            case 3:
+                mVelocidade.y = velocidade;
+                mVelocidade.x = -velocidade;
+                break;
+        }
+
+        // Velocidade para esquerda
+        mRb.velocity = mVelocidade;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log($"{this.tag} Colidiu");
+    }
+}
---
+++ Assets/Scripts/RacketController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+
+public class RacketController : MonoBehaviour
+{
+    // Posição da raquete
+    private Vector3 mPosition;
+    public float mY;
+    private float velocity = 3f;
+    private float limit = 3.5f;
+
+    // Se trata do jogador 1?
+    public bool player1 = false;
+
+    // Deve ser controlado pela AI?
+    public bool isAi = false;
+
+    void Start()
+    {
+        // Posição inicial da raquete
+        mPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        racketControls();
+    }
+
+    public void racketControls()
+    {
+        // Passando a posição do mY
+        mPosition.y = mY;
+
+        // Modificar a posição da raquete
+        transform.position = mPosition;
+
+        // Velocidade com deltatime
+        float velocidadeDelta = velocity * Time.deltaTime;
+
+        if (player1)
+        {
+            // Subindo a raquete do jogador 1
+            if (Input.GetKey(KeyCode.W) && mY < limit)
+            {
+                mY += velocidadeDelta;
+                Debug.Log($"Movendo a raquete do jogador 1 para cima");
+            }
+
+            // Descendo a raquete do jogador 1
+            if (Input.GetKey(KeyCode.S) && mY > -limit)
+            {
+                mY -= velocidadeDelta;
+
+                Debug.Log($"Movendo a raquete do jogador 1 para baixo");
+            }
+        }
+        else
+        {
+            if (!isAi)
+            {
+                // Subindo a raquete do jogador 2
+                if (Input.GetKey(KeyCode.UpArrow) && mY < limit)
+                {
+                    mY += velocidadeDelta;
+
+                    Debug.Log($"Movendo a raquete do jogador 2 para cima");
+                }
+
+                // Descendo a raquete do jogador 2
+                if (Input.GetKey(KeyCode.DownArrow) && mY > -limit)
+                {
+                    mY -= velocidadeDelta;
+
+                    Debug.Log($"Movendo a raquete do jogador 2 para baixo");
+                }
+            }
+            
+
+        }
+
+        // Impedir que o jogador sai da tela por baixo
+        if (mY < -limit)
+        {
+            mY = -limit;
+        }
+
+        // Impedir que o jogador sai da tela por cima
+        if (mY > limit)
+        {
+            mY = limit;
+        }
+    }
+}
